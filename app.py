91afb8332c0c1fa630884fcad48ac7f0793f7199
ARTariqDev <tariqmahmood.dmp@gmail.com>
2025-08-18 01:30:03 +0500

    changed port

--- app.py
+++ app.py
@@ -216,4 +216,4 @@ def reply_comment():
     return jsonify({"message": "Reply added", "reply": reply})
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
